2270926b2330b448e9c56f2cbdde051d16c8b835
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-01-10 02:00:26 -0800

    playing around with middlewares. caching, gzip. not that etavta needs gzip.

--- wsgi.py
+++ wsgi.py
@@ -39,6 +39,73 @@ class ConstantsMiddleware(Middleware):
 CONSTANTS = {'LEGS': ALL_LEGS}
 
 
+class HTTPCacheMiddleware(Middleware):
+    cache_attrs = ('max_age', 's_maxage', 'no_cache', 'no_store',
+                   'no_transform', 'must_revalidate', 'proxy_revalidate',
+                   'public', 'private')
+    def __init__(self,
+                 max_age=None,
+                 s_maxage=None,
+                 no_cache=None,
+                 no_store=None,
+                 no_transform=None,
+                 must_revalidate=None,
+                 proxy_revalidate=None,
+                 public=None,
+                 private=None):
+        for attr in self.cache_attrs:
+            setattr(self, attr, locals()[attr])
+
+    def request(self, next):
+        resp = next()
+        if not hasattr(resp, 'cache_control'):
+            return resp
+        for attr in self.cache_attrs:
+            cache_val = getattr(self, attr, None)
+            if cache_val:
+                setattr(resp.cache_control, attr, cache_val)
+        return resp
+
+from gzip import GzipFile
+from StringIO import StringIO
+def compress(data, level=6):
+    out = StringIO()
+    f = GzipFile(fileobj=out, mode='wb', compresslevel=level)
+    f.write(data)
+    f.close()
+    return out.getvalue()
+
+
+class GzipMiddleware(Middleware):
+    def __init__(self, compress_level=6):
+        self.compress_level = compress_level
+
+    def request(self, next, request):
+        resp = next()
+        # something with Vary
+        if resp.content_encoding or not request.accept_encodings['gzip']:
+            return resp
+
+        if 'msie' in request.user_agent.browser:
+            if not (resp.content_type.startswith('text/') or
+                    'javascript' in resp.content_type):
+                return resp
+
+        if resp.is_streamed:
+            return resp  # TODO
+        else:
+            comp_content = compress(resp.data, self.compress_level)
+            if len(comp_content) >= len(resp.data):
+                return resp
+            resp.response = [comp_content]
+            resp.content_length = len(comp_content)
+
+        resp.content_encoding = 'gzip'
+        #if hasattr(resp, 'get_etag') and resp.get_etag()[0]:
+        #    resp.add_etag('gzip')
+        return resp
+
+
 def create_app(schedule_dir, template_dir):
     schedule = Schedule.from_directory(schedule_dir)
     resources = {'schedule': schedule,
@@ -46,8 +113,10 @@ def create_app(schedule_dir, template_dir):
     subroutes = [('/', home, 'station_list.html'),
                  ('/<path:station_name>', get_stops, 'stop_times.html'),
                  ('/favicon.ico', not_found)]
+
     mako_response = MakoRenderFactory(template_dir)
-    middlewares = [ConstantsMiddleware(**CONSTANTS)]
+    cc_mw = HTTPCacheMiddleware(max_age=30, must_revalidate=True)
+    middlewares = [ConstantsMiddleware(**CONSTANTS), GzipMiddleware(), cc_mw]
     subapp = Application(subroutes, resources, mako_response, middlewares)
 
     routes = [('/', subapp), ('/v2/', subapp)]
